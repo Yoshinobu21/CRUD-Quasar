440dde93de5362e6ce6c1692b1f93498ff9919a5
Enzo Hirai <enzoyoshinobu@gmail.com>
2024-03-26 14:28:03 -0300

    input de data e cor corrigidos no formulário

--- src/pages/FormPost.vue
+++ src/pages/FormPost.vue
@@ -6,9 +6,23 @@
           :rules="[val => val && val.length > 0 || 'Please type something']" />
         <q-input outlined v-model="form.details" label="Details" lazy-rules class="col-lg-6 col-xs-12"
           :rules="[val => val && val.length > 0 || 'Please type something']" />
-        <q-input v-model="form.bgcolor" label="Color" outlined clearable style="padding-bottom: 20px;">
+
+        <q-select v-model="form.bgcolor" label="Color" outlined clearable style="min-width: 150px" :options="colors" />
+
+        <q-input filled v-model="form.date" mask="date" :rules="['date']">
+          <template v-slot:append>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                <q-date v-model="form.date">
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Close" color="primary" flat />
+                  </div>
+                </q-date>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
         </q-input>
-        <q-input v-model="form.date" filled type="date" label="Event Date" />
+
         <div class="col-12 q-gutter-sm">
           <q-btn label="Salvar" color="primary" class="float-right" icon="save" type="submit"></q-btn>
           <q-btn label="Cancelar" color="white" class="float-right" text-color="primary" :to="{ name: 'home' }"></q-btn>
@@ -23,6 +37,7 @@ import { defineComponent, ref, onMounted } from 'vue'
 import postsService from 'src/services/posts'
 import { useQuasar } from 'quasar'
 import { useRouter, useRoute } from 'vue-router'
+import { today } from '@quasar/quasar-ui-qcalendar/src'
 export default defineComponent({
   name: 'FormPost',
   setup () {
@@ -33,9 +48,12 @@ export default defineComponent({
     const form = ref({
       title: '',
       details: '',
-      date: '',
+      date: ref(today().replace(/-/g, '/')),
       bgcolor: ''
     })
+    const colors = [
+      'blue', 'purple', 'green', 'yellow', 'red'
+    ]
 
     onMounted(async () => {
       if (route.params.id) {
@@ -70,7 +88,8 @@ export default defineComponent({
     return {
       form,
       onSubmit,
-      getPost
+      getPost,
+      colors
     }
   }
 })
